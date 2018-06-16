db41fc3c341079440e33e289588d297d2e368fb0
Arver1 <aactepanov@mail.ru>
2018-06-16 21:49:16 +0300

    like a redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Store from './store'
+import {createStore} from './redux'
 
 const inititialState = {count: 0};
 
-function updateState(state, action) {
+function reducer(state = {count: 0}, action) {
     switch(action.type) {
         case 'INCREMENT' : return {count: state.count + action.amount}
         case 'DECREMENT' : return {count: state.count - action.amount}
@@ -17,7 +17,7 @@ const incrementAction = {type: 'INCREMENT', amount: 1}
 const decrementAction = {type: 'DECREMENT', amount: 1}
 const resetAction = {type: 'RESET'}
 
-const store = new Store(updateState, inititialState)
+const store = createStore(reducer, inititialState)
 
 class Counter extends React.Component {
 
@@ -26,21 +26,22 @@ class Counter extends React.Component {
     }
 
     increment() {
-        store.update(incrementAction)
+        store.dispatch(incrementAction)
     }
 
     decrement() {
-        store.update(decrementAction)
+        store.dispatch(decrementAction)
     }
 
     reset() {
-        store.update(resetAction)
+        store.dispatch(resetAction)
     }
     render(){
+        const {count} = store.getState()
         return (
             <div className="rect__wrapper">
                 <button className="counter" onClick={() => this.decrement()}><span className="counter__toggle">-</span></button>
-                <span className="counter__result">{store.state.count}</span>
+                <span className="counter__result">{count}</span>
                 <button className="counter" onClick={() => this.reset()}><span className="counter__toggle">Res</span></button>
                 <button className="counter" onClick={() => this.increment()}><span className="counter__toggle">+</span></button>
             </div>
